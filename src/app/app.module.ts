f62dd0c479730470440ce091aae5f4011ec6cbb7
Bharat Jain <bharatjain7723@gmail.com>
2017-12-26 20:51:06 +0530

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule, Route } from '@angular/router';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { 
+        path: 'register', 
+        component: RegisterComponent
+      },
+      { 
+        path: 'welcome', 
+        component: WelcomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
